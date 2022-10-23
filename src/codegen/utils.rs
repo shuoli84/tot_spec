f17c3dda37d8976fc911685c0dd38c71f2887c04
shuo <shuoli84@gmail.com>
2022-10-23 23:35:03 +0800

    util: add ensure_empty_lines to make append new line easier

--- src/codegen/utils.rs
+++ src/codegen/utils.rs
@@ -21,3 +21,66 @@ pub fn multiline_prefix_with(content: &str, prefix: &str) -> String {
         .collect::<Vec<_>>()
         .join("\n")
 }
+
+/// ensure at least count empty lines
+#[allow(unused)]
+pub fn ensure_emtpy_lines(val: &mut String, count: usize) {
+    use std::fmt::Write;
+
+    if count == 0 {
+        return;
+    }
+
+    let mut empty_line_num = 0;
+
+    let mut last_char: Option<char> = None;
+    for c in val.chars().rev() {
+        match c {
+            '\n' => {
+                if let Some(last_char) = last_char {
+                    if last_char == '\n' {
+                        empty_line_num += 1;
+                    }
+                }
+                last_char = Some('\n');
+            }
+            _ => last_char = Some(c),
+        }
+    }
+
+    // special case for val's first char is \n
+    if let Some('\n') = last_char {
+        empty_line_num += 1;
+    }
+
+    if count > empty_line_num {
+        for _ in 0..count - empty_line_num {
+            writeln!(val).expect("write to string only fail oom");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ensure_empty_line_break() {
+        fn test_it(input: &str, count: usize, expect: &str) {
+            let mut val = input.to_string();
+            ensure_emtpy_lines(&mut val, count);
+            assert_eq!(val, expect.to_string());
+        }
+
+        for (input, count, expect) in &[
+            ("", 0, ""),
+            ("", 1, "\n"),
+            ("\n", 1, "\n"),
+            ("\n", 2, "\n\n"),
+            // 2 empty lines => 3 \n
+            ("foo\n", 2, "foo\n\n\n"),
+        ] {
+            test_it(input, *count, expect);
+        }
+    }
+}
